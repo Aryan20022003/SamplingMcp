8d2e288b097f3c970a8dc7195fac1a8a97667140
Aryan <aryan.@Aryans-MacBook-Air.local>
2025-05-24 13:28:43 +0530

    mcp sampling part 2

--- sampling.py
+++ sampling.py
@@ -7,7 +7,8 @@ from gmail import read_gmail_messages
 
 mcp = FastMCP("Gmail Assistant server")
 
-# ''' Basic concept of sampling here is server request the client to do LLM operation on the data instead of doing it on the server . Major benefit is end user might now be concerned about the data privacy and security.'''
+# ''' Basic concept of sampling here is server request the client to do LLM operation on the data instead of doing it on the server . Major benefit is end user might now be concerned about the data privacy and security.
+# Please refer readme for resource on sampling and how it works'''
 
 
 @mcp.tool()
@@ -17,14 +18,17 @@ def readGmail() -> types.SamplingMessage:
     return types.SamplingMessage(
         role="assistant",
         content=types.TextContent(type="text", text=json.dumps(messages, indent=2)),
-        systemPrompt="Understand the raw message and write nice summary of all the message for the user. Keep in mind the user is starting his day and he is looking for a summary of all the messages he received in his inbox. ",
+        systemPrompt="Understand the raw message and write nice summary of all the message for the user. Keep in mind the user is starting his day and he is looking for a summary of all the messages he received in his inbox. Write the summary in form of paragraphs and make sure to include the most important information from each.",
     )
 
 
 @mcp.tool()
 def processSummary(summary: str) -> str:
     """Process the summary of the email messages and return a final response with data following the company policy"""
-    str1 = summary + "aryan@company.com is the email of the user. Please make sure to follow the company policy and do not share any personal information. "
+    str1 = (
+        summary
+        + "aryan@company.com is the email of the user. Please make sure to follow the company policy and do not share any personal information. "
+    )
     return str1
 
 
